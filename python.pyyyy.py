53fe6ad8862f105eb79888529126b5843ee03a63
aruzhanaaaaaaa <158404030+aruzhanaaaaaaa@users.noreply.github.com>
2024-05-13 15:28:51 +0500

    Add files via upload

---
+++ python.pyyyy.py
@@ -0,0 +1,74 @@
+#attr - атрибут
+#class  A :
+    #attr = 'Helo world'
+    #def hello(self):
+        #print(self.attr)
+
+
+#сlass  A :
+   ## attr = 'Helo world'
+    #def hello(self):
+        #print(self.attr)
+
+#obj1 = A()
+#hello = obj1.hello()
+#print(hello)
+
+
+#class Person:
+    #def __init__(self,name,age):
+        #self.name = name
+        #self.age = age
+
+
+    #def my_method(self):
+        #print("Менің атым "+ self.name)
+#p1 = Person("Dauren",18)
+#p1.my_method()
+
+
+#class Person:
+    #def __init__(self,name,age):
+        #self.name = name
+       # self.age = age
+
+
+    #def my_method(self):
+        #print("Менің атым "+ self.name, "жасым " ,self.age, "де")
+#p1 = Person("Dauren",18)
+#p1.my_method()
+
+
+class Person:
+    def __init__(self, name, age, surname):
+        self.name = name
+        self.age = age
+        self.surname = surname
+
+    def my_method(self):
+        print("Менің атым " + self.name, "жасым ", self.age, "және тегім ", self.surname, "де")
+
+p1 = Person("Аружан", 18, "Исалдаева")
+p1.my_method()
+
+
+
+class Person:
+    def __init__(self, name, age, surname=""):
+        self.name = name
+        self.age = age
+        self.surname=surname
+
+        
+    def my_method(self):
+        if self.surname:
+            print("Менің атым "+ self.name, self.surname ,"жасым ", self.age, )
+            
+        else:
+            print("Менің атым "+ self.name,"жасым " ,self.age, "де " )
+       
+
+p1 = Person("Аружан",18,"Исалдаева")
+p1.my_method()
+p1 = Person("Аружан",18,"")
+p1.my_method()
